5c6aaf7c396c10c01d7bf91f678fb33eb13e8f5e
Gutemberg Nunes Freitas do Nascimento <g.a.do.nascimento@avanade.com>
2022-07-11 17:28:22 -0300

    CalculadoraMVC2

---
+++ CalculadoraMVC2/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using CalculadoraMVC2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CalculadoraMVC2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Calculadora cal)
+        {
+            int a, b;
+            a = cal.Num1;
+            b = cal.Num2;
+            if (cal.Operador == "   subtrair   ")
+            {
+                cal.Resultado = a - b;
+            }
+            else if (cal.Operador == " multiplicar ")
+            {
+                cal.Resultado = a * b;
+            }
+            else if (cal.Operador == "     dividir     ")
+            {
+                cal.Resultado = a / b;
+            }
+            else
+            {
+                cal.Resultado = a + b;
+            }
+            ViewData["result"] = cal.Resultado;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CalculadoraMVC2/Models/Calculadora.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraMVC2.Models
+{
+    public class Calculadora
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Resultado { get; set; }
+        public string? Operador { get; set; }
+    }
+}
